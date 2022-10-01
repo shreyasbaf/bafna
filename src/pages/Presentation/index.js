895e6cd42bb4b1968c1d5ad1331f27fd716c2f37
shreyasbaf <shreyas7bafna@gmail.com>
2022-10-02 00:58:11 +0530

    homepag

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -30,7 +30,7 @@ import FilledInfoCard from "examples/Cards/InfoCards/FilledInfoCard";
 
 // Presentation page sections
 import Counters from "pages/Presentation/sections/Counters";
-import Information from "pages/Presentation/sections/Information";
+// import Information from "pages/Presentation/sections/Information";
 import DesignBlocks from "pages/Presentation/sections/DesignBlocks";
 import Pages from "pages/Presentation/sections/Pages";
 import Testimonials from "pages/Presentation/sections/Testimonials";
@@ -110,7 +110,7 @@ function Presentation() {
         }}
       >
         <Counters />
-        <Information />
+        {/* <Information /> */}
         <DesignBlocks />
         <Pages />
         <Container sx={{ mt: 6 }}>
